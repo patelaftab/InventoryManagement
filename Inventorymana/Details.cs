b418d2b000e4b99f35b292ef30190e12082152c2
patelaftab <10patelaftab@gmail.com>
2022-12-23 18:38:21 +0530

    Cheking oops PArt 2 with jason mathod

---
+++ Inventorymana/Details.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventorymana
+{
+  public class Details
+    {
+        public List<TypesOfRice> typesOfRice;
+    }
+    public class TypesOfRice 
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+}
---
+++ Inventorymana/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Inventorymana
+{
+     class program
+    {
+        public static void Main(string[] args)
+        {
+            string path = @"C:\Users\Dell\source\repos\Inventorymana\Inventorymana\Inventory.json";
+            FetchInventoryDetails fetchInventoryDetails=new FetchInventoryDetails();
+            Details data =fetchInventoryDetails.Read(path);
+            for (int i = 0; i < data.typesOfRice.Count; i++)
+            {
+                Console.WriteLine(data.typesOfRice[i].name);
+                Console.WriteLine(data.typesOfRice[i].price);
+                Console.WriteLine(data.typesOfRice[i].weight);
+            }
+           
+        }
+    }
+}
