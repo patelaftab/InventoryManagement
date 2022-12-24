f42991ab580bdbf91a10b48cd5b2a6e1aca494a4
patelaftab <10patelaftab@gmail.com>
2022-12-24 08:11:32 +0530

    Cheking a Total price for WHeat,RIce and Pulses

--- Inventorymana/Details.cs
+++ Inventorymana/Details.cs
@@ -9,8 +9,22 @@ namespace Inventorymana
   public class Details
     {
         public List<TypesOfRice> typesOfRice;
+        public List<TypesOfWheat> typesOfwheat;
+        public List<TypesOfPulses> typesOfpulses;
     }
     public class TypesOfRice 
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+    public class TypesOfWheat
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+    public class TypesOfPulses
     {
         public string name;
         public int weight;
--- Inventorymana/Program.cs
+++ Inventorymana/Program.cs
@@ -12,8 +12,28 @@
                 Console.WriteLine(data.typesOfRice[i].name);
                 Console.WriteLine(data.typesOfRice[i].price);
                 Console.WriteLine(data.typesOfRice[i].weight);
+                int Totalprice = data.typesOfRice[i].weight * data.typesOfRice[i].price;
+                Console.WriteLine("The Price For " + data.typesOfRice[i].weight + "kg is :" + Totalprice);
+                Console.WriteLine("-----------------------------------------");
             }
-           
+            for (int i = 0; i < data.typesOfwheat.Count; i++)
+            {
+                Console.WriteLine(data.typesOfwheat[i].name);
+                Console.WriteLine(data.typesOfwheat[i].price);
+                Console.WriteLine(data.typesOfwheat[i].weight);
+                int Totalprice = data.typesOfwheat[i].weight * data.typesOfwheat[i].price;
+                Console.WriteLine("The Price For " + data.typesOfwheat[i].weight + "kg is :" + Totalprice);
+                Console.WriteLine("-----------------------------------------");
+            }
+            for (int i = 0; i < data.typesOfpulses.Count; i++)
+            {
+                Console.WriteLine(data.typesOfpulses[i].name);
+                Console.WriteLine(data.typesOfpulses[i].price);
+                Console.WriteLine(data.typesOfpulses[i].weight);
+                int Totalprice = data.typesOfpulses[i].weight * data.typesOfpulses[i].price;
+                Console.WriteLine("The Price For " + data.typesOfpulses[i].weight + "kg is :" + Totalprice);
+                Console.WriteLine("-----------------------------------------");
+            }
         }
     }
 }
